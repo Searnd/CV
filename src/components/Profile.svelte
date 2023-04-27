64879fa6dae27f3cd4d7bbe7279323ecafb24818
Andres Flores <andres.flores@polymtl.ca>
2023-04-26 22:47:09 -0400

    works

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import boringProfilePicture from "../assets/pfp.jpg";
-  import funProfilePicture from "../assets/pfp0.jpg";
+  import boringProfilePicture from "../assets/pfp0.jpg";
+  import funProfilePicture from "../assets/pfp1.jpg";
 
   let isFunMode = false;
 
   $: myName = isFunMode ? "Andres Flores 🤙" : "Andres Flores";
+  $: profilePicture = isFunMode ? funProfilePicture : boringProfilePicture;
 
   function toggleFunMode() {
     isFunMode = !isFunMode;
@@ -12,18 +13,12 @@
 </script>
 
 <div class="profile-area">
-  <div on:click={toggleFunMode} class="flip-outer" class:fun={isFunMode}>
-    <div class="flip-inner">
-      {#if !isFunMode}
-      <div class="flip-front">
-        <img id="profile-picture" src="{boringProfilePicture}" alt="profile">
-      </div>
-      {:else}
-      <div class="flip-back">
-        <img id="profile-picture" src="{funProfilePicture}" alt="profile">
-      </div>
-      {/if}
-    </div>
+  <div class="profile-picture-container" class:back={isFunMode}>
+    <img
+      on:click={toggleFunMode}
+      class:back={isFunMode}
+      class="profile-picture"
+      src="{profilePicture}" alt="profile">
   </div>
   <p id="my-name">{myName}</p>
 </div>
@@ -31,40 +26,30 @@
 <style lang="scss">
   .profile-area {
     width: 25%;
-  }
-  .flip-outer {
-    perspective: 1000px;
-    &:hover {
-      cursor: pointer;
-    }
-    &.fun {
-      .flip-inner {
-        transform: rotateY(180deg);
-      }  
-    }
-
-    .flip-inner {
-      transition: transform 0.6s;
-      transform-style: preserve-3d;
-
-      .flip-front, .flip-back {
-        -webkit-backface-visibility: hidden;
-        backface-visibility: hidden;
-      }
-
-      .flip-back {
-        transform: rotateY(180deg);
-      }
-    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
-  #profile-picture {
+  .profile-picture-container {
+    transition: transform 0.6s;
+  }
+
+  .profile-picture {
     object-fit: cover;
     border-radius: 50%;
     height: 100px;
     width: 100px;
+    transition: transform 0.6s;
+
+    &:hover {
+      cursor: pointer;
+    }
   }
 
+  .back {
+    transform: rotateY(180deg);
+  }
   #my-name {
     margin: 0;
     font-size: 1.25em;
